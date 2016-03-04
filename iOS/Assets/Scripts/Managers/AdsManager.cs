5e193ba9f2d9a56d5a79f5c37c500f44c2e603c6
Andrew Chumchal <andyipod1437@gmail.com>
2016-03-04 08:07:07 -0600

    Move Files
    Move iOS and Andriod

---
+++ iOS/Assets/Scripts/Managers/AdsManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour {
+
+	public string gameId; // Set this value from the inspector.
+	public bool enableTestMode = true;
+
+	void Start ()
+	{
+		Advertisement.Initialize ("1041968", true);
+
+		//StartCoroutine (ShowAdWhenReady ());
+	}
+
+	IEnumerator ShowAdWhenReady()
+	{
+		while (!Advertisement.IsReady ())
+			yield return null;
+
+		Advertisement.Show ();
+	}
+}
---
+++ iOS/Assets/Scripts/TennisBall.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TennisBall : MonoBehaviour
+{
+	public float speed = 2f;
+	private SpriteRenderer spriteRenderer;
+	public Rigidbody2D[] rb;
+
+	void Start()
+	{
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+	}
+
+	void Update () 
+	{
+		transform.position += Time.deltaTime * speed * transform.forward;
+		if(spriteRenderer.isVisible == false)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D aCollider)
+	{
+		if(aCollider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+		{
+			scoreManager.currentScore += dogAttack.dogValue;
+			Destroy(aCollider.gameObject);
+			//Destroy(gameObject);
+
+		}
+	}
+}
