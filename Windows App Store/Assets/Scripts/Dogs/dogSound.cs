6f8e3a82e0facbd9bb3d62fa47c0656c305761e2
Andrew Chumchal <andyipod1437@gmail.com>
2016-07-20 16:17:39 -0700

    Added Windows Store

---
+++ Windows App Store/Assets/Scripts/Dogs/dogSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class dogSound : MonoBehaviour {
+
+    public AudioSource dogHit;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+       
+	}
+}
